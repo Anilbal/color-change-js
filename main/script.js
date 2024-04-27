c4c791214c55ce5fc941a39985325263a65b87e2
Anilbal <139449166+Anilbal@users.noreply.github.com>
2024-04-27 13:40:06 +0000

    completed projects

---
+++ main/script.js
@@ -0,0 +1,22 @@
+//generating random color
+const randomColor=function(){
+    const hex='0123456789ABCDEF'
+    let color="#"
+    for(let i=0;i<6;i++){
+        color +=hex[Math.floor(Math.random()*16)]
+    }
+    return color
+}
+
+let intervalId
+document.querySelector('#start').addEventListener('click',()=>{
+    if(!intervalId){
+        intervalId=setInterval(()=>{
+            document.body.style.backgroundColor=randomColor();
+        },1000)
+    }
+})
+document.querySelector('#stop').addEventListener('click',()=>{
+    clearInterval(intervalId)
+    intervalId=null;
+})
